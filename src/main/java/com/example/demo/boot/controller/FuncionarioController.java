0fa7a89b361993628fb076cdf5931e0415a8274f
leocassiosilva <leocassiosilva1234@gmail.com>
2020-08-03 18:39:50 -0300

    metodo buscar funcionario por nome

--- src/main/java/com/example/demo/boot/controller/FuncionarioController.java
+++ src/main/java/com/example/demo/boot/controller/FuncionarioController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.boot.domain.Cargo;
@@ -68,6 +69,13 @@ public class FuncionarioController {
 		attr.addAttribute("sucess", "Funcionario excluido com sucesso!"); 
 		return "redirect:/funcionarios/listar";
 	}
+	
+	@GetMapping("/buscar/nome")
+	public String getPorNome(@RequestParam("nome") String nome, ModelMap model) {
+		model.addAttribute("funcionario", funcionarioService.buscarPorNome(nome));
+		return "redirect:/funcionarios/listar";
+	}
+	
 	
 	@ModelAttribute("cargos")
 	public List<Cargo> getCargos(){
--- src/main/java/com/example/demo/boot/service/FuncionarioServiceImpl.java
+++ src/main/java/com/example/demo/boot/service/FuncionarioServiceImpl.java
@@ -42,6 +42,11 @@ public class FuncionarioServiceImpl implements FuncionarioService{
 	public List<Funcionario> buscarTodos() {
 		return dao.findAll();
 	}
+
+	@Override
+	public List<Funcionario> buscarPorNome(String nome) {
+		return dao.findByNome(nome);
+	}
 	
 	
 	
